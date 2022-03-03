9c92c2902f7ca4265e3b95567c003f9fe0101a5c
vritantj24 <vritantjain2001@gmail.com>
2022-03-03 18:42:06 +0530

    Added Swipe to Delete Feature

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.notes
 
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), INotesRVAdapter {
 
-    lateinit var viewModel: NoteViewModel
+    private lateinit var viewModel: NoteViewModel
+    private lateinit var adapter : NotesRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView.layoutManager= LinearLayoutManager(this)
-        val adapter = NotesRVAdapter(this,this)
+        adapter = NotesRVAdapter(this,this)
         recyclerView.adapter=adapter
 
         viewModel= ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
@@ -28,6 +35,53 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
              }
         })
 
+        val itemTouchHelper = ItemTouchHelper(simpleCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+
+    }
+
+    private val simpleCallback = object : ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+    {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            val note = adapter.allNotes[position]
+            viewModel.deleteNote(note)
+
+            Snackbar.make(recyclerView,"${note.text} Deleted",Snackbar.LENGTH_LONG)
+                .setAction("Undo",View.OnClickListener {
+                    viewModel.insertNote(note)
+                    Toast.makeText(this@MainActivity,"${note.text} Inserted",Toast.LENGTH_SHORT).show()
+                }).show()
+
+        }
+
+        override fun onChildDraw(
+            c: Canvas,
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            dX: Float,
+            dY: Float,
+            actionState: Int,
+            isCurrentlyActive: Boolean
+        ) {
+            RecyclerViewSwipeDecorator.Builder(c,recyclerView,viewHolder,dX, dY, actionState, isCurrentlyActive)
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.orange))
+                .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_forever_24)
+                .addSwipeRightBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.orange))
+                .addSwipeRightActionIcon(R.drawable.ic_baseline_delete_forever_24)
+                .create()
+                .decorate()
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+        }
     }
 
     override fun onItemClicked(note: Note) {
@@ -36,11 +90,11 @@ class MainActivity : AppCompatActivity(), INotesRVAdapter {
     }
 
     fun submitData(view: View) {
-        val noteText = input.text.toString()
+        val noteText = input.text.toString().trim()
         if(noteText.isNotEmpty())
         {
             viewModel.insertNote(Note(noteText))
-            Toast.makeText(this,"${noteText} Inserted",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"$noteText Inserted",Toast.LENGTH_SHORT).show()
             input.setText("")
         }
     }
--- app/src/main/java/com/example/notes/NotesRVAdapter.kt
+++ app/src/main/java/com/example/notes/NotesRVAdapter.kt
@@ -2,22 +2,20 @@ package com.example.notes
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.OrientationEventListener
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NotesRVAdapter(private val context: Context, private val listener: INotesRVAdapter): RecyclerView.Adapter<NotesRVAdapter.NoteViewHolder>() {
 
-    private val allNotes= ArrayList<Note>()
+    val allNotes= ArrayList<Note>()
 
     inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
-        val textView: TextView = itemView.findViewById<TextView>(R.id.text)
-        val deleteButton: ImageView = itemView.findViewById<ImageView>(R.id.deleteButton)
+        val textView: TextView = itemView.findViewById(R.id.text)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
